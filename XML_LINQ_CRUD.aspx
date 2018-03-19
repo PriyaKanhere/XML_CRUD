1920061f9e3951e766b31dc1806fbe89bb2631aa
Priya Kanhere <31685615+PriyaKanhere@users.noreply.github.com>
2018-03-19 15:55:42 +0530

    Initial commit

---
+++ XML_LINQ_CRUD.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="XML_LINQ_CRUD.aspx.cs" Inherits="XML_CRUD.XML_LINQ_CRUD" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 162px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:GridView ID="GridView1" runat="server" Height="123px" Width="583px">
+        </asp:GridView>
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">&nbsp;&nbsp; ID&nbsp;</td>
+                <td>
+                    <asp:TextBox ID="txtid" runat="server" Height="16px" Width="147px"></asp:TextBox>
+                    <asp:Button ID="Button2" runat="server" OnClick="Find" Text="Find By ID" />
+&nbsp;
+                    <asp:Button ID="Button5" runat="server" OnClick="delete_click" Text="Delete By ID" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;&nbsp; Name&nbsp;</td>
+                <td>
+                    <asp:TextBox ID="txtname" runat="server" Height="16px" Width="147px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;&nbsp; Salary&nbsp;</td>
+                <td>
+                    <asp:TextBox ID="txtsalary" runat="server" Height="16px" Width="147px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;&nbsp; Email&nbsp;</td>
+                <td>
+                    <asp:TextBox ID="txtemail" runat="server" Height="16px" Width="147px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp; Address&nbsp;</td>
+                <td>
+                    <asp:TextBox ID="txtaddress" runat="server" Height="16px" Width="147px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Insert_Click" Text="Insert" />
+&nbsp;&nbsp;
+                    <asp:Button ID="Button4" runat="server" OnClick="Update_click" Text="Update" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp; </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ XML_LINQ_CRUD.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace XML_CRUD
+{
+    public partial class XML_LINQ_CRUD : System.Web.UI.Page
+    {
+        XDocument xmldoc;
+        public void BindGrid()
+        {
+            //Load xml document and bind it to grid
+            xmldoc = XDocument.Load(@"C:\GitHub\XML_CRUD\XML_CRUD\XML_CRUD\Employee.xml");
+            var bind = xmldoc.Descendants("Employee").Select(p => new
+            {
+                Id = p.Element("id").Value,
+                Name = p.Element("name").Value,
+                Salary = p.Element("salary").Value,
+                Email = p.Element("email").Value,
+                Address = p.Element("address").Value
+            }).OrderBy(p => p.Id);
+            GridView1.DataSource = bind;
+            GridView1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+        protected void Find(object sender, EventArgs e)
+        {
+            //Find Employee details on the basis on Employee ID
+            XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("id").Value == txtid.Text);
+            if (emp != null)
+            {
+                txtname.Text = emp.Element("name").Value;
+                txtsalary.Text = emp.Element("salary").Value;
+                txtemail.Text = emp.Element("email").Value;
+                txtaddress.Text = emp.Element("address").Value;
+
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+            //Insert in xml document
+            XElement emp = new XElement("Employee",
+                new XElement("id", txtid.Text),
+                new XElement("name", txtname.Text),
+                new XElement("salary", txtsalary.Text),
+                new XElement("email", txtemail.Text),
+                new XElement("address", txtaddress.Text));
+            xmldoc.Root.Add(emp);
+            xmldoc.Save(@"C:\GitHub\XML_CRUD\XML_CRUD\XML_CRUD\Employee.xml");
+            BindGrid();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            txtid.Text = "";
+            txtname.Text = "";
+            txtsalary.Text = "";
+            txtaddress.Text = "";
+            txtemail.Text = "";
+            txtid.Focus();
+        }
+
+        private void Update(XElement emp)
+        {
+            if (emp != null)
+            {
+                emp.Element("name").Value = txtname.Text;
+                emp.Element("salary").Value = txtsalary.Text;
+                emp.Element("email").Value = txtemail.Text;
+                emp.Element("address").Value = txtaddress.Text;
+                xmldoc.Root.Add(emp);
+                xmldoc.Save(@"C:\GitHub\XML_CRUD\XML_CRUD\XML_CRUD\Employee.xml");
+                BindGrid();
+                Reset();
+
+            }
+        }
+
+        private XElement Delete()
+        {
+            XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("id").Value == txtid.Text);
+            if (emp != null)
+            {
+                emp.Remove();
+                xmldoc.Save(@"C:\GitHub\XML_CRUD\XML_CRUD\XML_CRUD\Employee.xml");
+                BindGrid();
+            }
+            return emp;
+        }
+        protected void Update_click(object sender, EventArgs e)
+        {
+            //First delete the old record and then add the new record
+           XElement emp = Delete();
+            Update(emp);
+        }
+        
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void delete_click(object sender, EventArgs e)
+        {
+            
+            Delete();
+            Reset();
+        }
+    }
+}
